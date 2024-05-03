d0936ffc1ca1050e1d7400a7839ed64ad2c779c7
Hezron Nyamboga <hezzyn1@gmail.com>
2024-05-03 04:03:28 +0300

    router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/Contact";
+import Services from "./pages/Services";
+import Pricing from "./pages/Pricing";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<App />} />
+      <Route path='/aboutUs' element={<AboutUs />} />
+      <Route path='/contactUs' element={<ContactUs />} />
+      <Route path='/services' element={<Services />} />
+      <Route path='/pricing' element={<Pricing />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
