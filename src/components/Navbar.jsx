0942fe7f8cbd411e8a17492ef9f32540690c91e2
Hezron Nyamboga <hezzyn1@gmail.com>
2024-05-04 01:21:35 +0300

    update navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,25 @@
 import { Link } from "react-router-dom";
 import Links from "./data";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
   return (
     <nav className='navbar'>
       <h2 className='logo'>
         <Link to='/'>EddieClean</Link>
       </h2>
-      <input type='checkbox' id='menu-toggle' />
+      <input
+        type='checkbox'
+        checked={isNavbarOpen}
+        id='menu-toggle'
+        onChange={(e) => {
+          const { checked } = e.target;
+          setIsNavbarOpen(checked);
+        }}
+      />
       <label htmlFor='menu-toggle' id='hamburger-btn'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          height='24'
-          viewBox='0 0 24 24'
-          width='24'
-        >
-          <path
-            d='M3 12h18M3 6h18M3 18h18'
-            stroke='currentColor'
-            stroke-width='2'
-            stroke-linecap='round'
-          />
-        </svg>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          fill='currentColor'
-        >
-          <path d='M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z'></path>
-        </svg>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          fill='currentColor'
-        >
-          <path d='M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z'></path>
-        </svg>
+        {!isNavbarOpen ? <FaBars /> : <FaTimes style={{ color: "#a12e2e" }} />}
       </label>
 
       <ul className='links'>
